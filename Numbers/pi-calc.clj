5deed671601af6190a1fec16914ddb45578de8ee
Daniel Chyan <dchyan1@gmail.com>
2013-08-03 17:12:29 -0400

    kinda works

--- Numbers/pi-calc.clj
+++ Numbers/pi-calc.clj
@@ -3,18 +3,19 @@
 
 (def A [])
 (def init-value 2)
-(def init-size (+ (* 10 (double (/ n-value  3)) 1)))
+(def init-size (+ (* 10 (int (with-precision 100 :FLOOR (/ n-value 3)))) 1))
 
 (defn regular-form [orig]
   (def result ())
   (conj result (last orig))
   (def q 0)
-  (def i (int (+ init-size 1)))
+  (def i (int (+ init-size 0)))
   (def testing 1)
- (doseq [cur-elem (rseq orig)]
+ (doseq [cur-elem (reverse orig)]
+    ;(prn "========")
     ;(prn "i = " i)
     ;(prn "cur-elem" cur-elem)
-    (def x (+ (* 10 cur-elem) (* q i))) 
+    (def x (+ (* 10 cur-elem) (* q i)))
     ;(prn "x = " x)
     (def mod-elem (mod x (- (* 2 i) 1)))
     ;(prn "mod-elem = " mod-elem)
@@ -22,7 +23,9 @@
     (def q (int (/ x (- (* 2 i) 1))))
     ;(prn "q = " q)
     (def i (- i 1))
+    ;(prn "========")
  )
+  ;(println "Return q = " q)
   [result q]
   )
 
@@ -42,30 +45,55 @@
 (doseq [j (range n-value)]
     (def combo (regular-form A))
     (def A (first combo))
+    (def A (conj (rest A) (mod (last combo) 10)))
     (def q (int (/ (last combo) 10)))
-    (def A (conj (rest A) (mod (first A) 10)))
 
-    (if (= 9 q) 
-        ((def nines (+ nines 1))
-            (if (= 10 q)
-                (printf "%d" (+ predigit 1))
-                (doseq [i (range nines)]
-                    (printf "%d" 0)
-                )
-                (def predigit 0)
-                (def nines 0)
-            )
+    ;(println "A =" A)
+    ;(println "q =" q)
+    ;(println "nines = " nines)
+    ;(println "predigit = " predigit)
+
+    (def state-check (cond 
+        (= 9 q) "state1"
+        (= 10 q) "state2"
+        :else "state3"
+    ))
+
+    (if (= state-check "state1")
+        (do 
+            ;(println "state1")
+            (def nines (+ nines 1))
         )
-        ((printf "%d" predigit)
-            (def predigit q)
-            (if-not (= 0 nines) 
-                (doseq [i (range nines)]
-                    (printf "%d" 9)
+    )
 
+    (if (= state-check "state2")
+        ( do
+            ;(println "state2")
+            (printf "%d" (+ predigit 1))
+            (doseq [i (range nines)]
+                (printf "%d" 0)
+            )
+            (def predigit 0)
+            (def nines 0)
+        )
+    )
+
+    (if (= state-check "state3")
+        ( do
+            ;(println "state3")
+            (printf "%d" predigit)
+            (def predigit q)
+            
+            (if (not(zero? nines))
+                ( do
+                    ;(println nines)
+                    (doseq [k (range nines)]
+                        (printf "%d" 9)
+                    )
                 )
             )
             (def nines 0)
         )
     )
 )
-printf "%d" predigit
+(printf "%d" predigit)
